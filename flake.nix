2efd53b3a4cc379b59968611e06396f98fc46854
rw <rw@jjk.is>
2024-02-18 20:39:13 -0800

    build with nix

--- flake.nix
+++ flake.nix
@@ -17,14 +17,37 @@
             buildInputs = with pkgs; [
               nodejs
               yarn
+              nodePackages.node2nix
+
             ];
+            shellHook = ''
+              export NODE_OPTIONS=--openssl-legacy-provider
+            '';
+          };
+          nodeDependencies = (pkgs.callPackage ./default.nix { }).nodeDependencies;
+
+
+          app = pkgs.stdenv.mkDerivation {
+            name = "my-app";
+            src = ./.;
+            buildInputs = [ 
+              pkgs.nodejs 
+            ];
+            buildPhase = ''
+              ln -s ${nodeDependencies}/lib/node_modules ./node_modules
+              export PATH="${nodeDependencies}/bin:$PATH"
+              # Build the distribution bundle in "dist"
+              npm run build
+              cp -r src/.vuepress/dist $out/
+            '';
+            NODE_OPTIONS="--openssl-legacy-provider";
           };
 
 
         in
         {
-          # app = app;
-          # packages.default = app;
+          app = app;
+          packages.default = app;
           devShells.default = devshell;
 
         })
